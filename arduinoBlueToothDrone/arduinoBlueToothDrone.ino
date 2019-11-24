1599a92c7da4a2541e87fd57a597733247ee1318
druhela <druhela@edu.uwaterloo.ca>
2019-11-24 16:00:42 -0500

    updated arduino code with new vals

--- arduinoBlueToothDrone/arduinoBlueToothDrone.ino
+++ arduinoBlueToothDrone/arduinoBlueToothDrone.ino
@@ -3,11 +3,21 @@
  * works with all 4 motors connected in parallel with the bread board
  */
 #include <Servo.h>
-Servo s3,s4;
-int high3 = 30;
-int low3 = 70;
-int high4 = 10;
-int low4 = 90;
+int speedSet = 0;
+
+Servo s1,s2,s3,s4;
+
+int high1 = 70;
+int low1 = 10;
+
+int high2 = 90;
+int low2 = 10;
+
+int high3 = 90;
+int low3 = 10;
+
+int high4 = 90;
+int low4 = 10;
 char command = '0'; 
 
 void setSpeed(int speed, Servo s)
@@ -27,52 +37,56 @@ void arm()
 //m2: 10 - 90
 //m3: 20 - 80
 //m4: DNE
-/*Serial.println("Arming S1");
-setSpeed(20,s1);
-delay(1000);
-
-setSpeed(80,s1);
-delay(1000);
-Serial.println("Armed S1");
-Serial.println("Arming S2");
-setSpeed(20,s2);
-delay(1000);
-
-setSpeed(80,s2);
-delay(1000);
-Serial.println("Armed S2");
-Serial.println("Arming S3");
-setSpeed(20,s3);
-delay(1000);
-
-setSpeed(80,s3);
-delay(1000);
-Serial.println("Armed S3");*/
+//ARMING 4
 delay(5000);
 Serial.println("Arming S4");
 Serial.println("Arm LOW S4");
-setSpeed(low4,s4);
+setSpeed(high4,s4);
 delay(5000);
 Serial.println("Arm HIGH S4");
-setSpeed(high4,s4);
+setSpeed(low4,s4);
 delay(5000);
 Serial.println("Armed S4");
 
 delay(1000);
 
+//ARMING 3
 Serial.println("Arming S3");
 Serial.println("Arm LOW S3");
-setSpeed(low3,s3);
+setSpeed(high3,s3);
 delay(5000);
 Serial.println("Arm HIGH S3");
-setSpeed(high3,s3);
+setSpeed(low3,s3);
 delay(5000);
 Serial.println("Armed S3");
 
-//setSpeed(30,s3);
+//ARMING 2
+delay(5000);
+Serial.println("Arming S2");
+Serial.println("Arm LOW S2");
+setSpeed(high2,s2);
+delay(5000);
+Serial.println("Arm HIGH S2");
+setSpeed(low2,s2);
+delay(5000);
+Serial.println("Armed S2");
+
+//ARMING 1
+delay(1000);
+Serial.println("Arming S1");
+Serial.println("Arm LOW S1");
+setSpeed(high1,s1);
+delay(5000);
+Serial.println("Arm HIGH S1");
+setSpeed(low1,s1);
+delay(5000);
+Serial.println("Armed S1");
+
 Serial.println("Rest Phase Started");
 setSpeed((high4+low4)/4,s4);
-setSpeed((high3+low3)/4,s3);
+setSpeed(35,s3);
+setSpeed((high2+low2)/2,s2);
+setSpeed((high1+low1)/2,s1);
 delay(5000);
 Serial.println("Rest Phase Complete");
 }
@@ -82,11 +96,10 @@ void setup()
   // initialize the serial port:
   Serial.begin(9600);
   
-  /*s1.attach(6);
+  s1.attach(6);
   s2.attach(9);
-  s3.attach(10);*/
-  s3.attach(9);
-  s4.attach(10);
+  s3.attach(10);
+  //s4.attach(11);
   arm();
 }
 
@@ -97,61 +110,23 @@ void loop() {
      command = Serial.read();
      Serial.println(command);
      if(command == '1' || command == '!'){
+       Serial.println("COMMAND RECEIVED");
+        Serial.println("Starting");
+        
+        setSpeed(speedSet,s1);
+        setSpeed(speedSet,s2);
+        setSpeed(speedSet,s3);
+        setSpeed(speedSet,s4);
+        speedSet += 3;
+        delay(1000);
+     } else if(command == '4' || command == '$'){
         Serial.println("COMMAND RECEIVED");
         Serial.println("Stopping");
+        speedSet =0;
+        setSpeed(0,s1);
+        setSpeed(0,s2);
         setSpeed(0,s3);
         setSpeed(0,s4);
-        delay(1000);
-        /*int speed;
-       
-        Serial.println("Sweeping up");
-        for(speed = low; speed <= high; speed+= 1)
-        {
-          //setSpeed(speed,s3);
-          setSpeed(speed,s4);
-          Serial.println(speed);
-          delay(100);
-        }
-  
-        //setSpeed(20,s3);
-        setSpeed((high+low)/2,s4);
-        
-        delay(10000);
-  
-        Serial.println("Sweeping down");
-        for(speed = high; speed >low; speed -=1)
-        {
-          //setSpeed(speed,s3);
-          setSpeed(speed,s4);
-          Serial.println(speed);
-          delay(100);
-        }
-  
-        Serial.println("30 halting...");
-        //setSpeed(20,s3);
-        setSpeed((high+low)/2,s4);
-        
-        delay(5000);*/
-     } else if(command == '3' || command == '#'){
-        Serial.println("COMMAND RECEIVED");
-        Serial.println("Starting");
-        setSpeed(20,s3);
-        setSpeed(20,s4);
-        delay(1000);
-        setSpeed(30,s3);
-        setSpeed(30,s4);
-        delay(1000);
-        setSpeed(40,s3);
-        setSpeed(40,s4);
-        delay(1000);
-        setSpeed(50,s3);
-        setSpeed(50,s4);
-        delay(1000);
-        setSpeed(60,s3);
-        setSpeed(60,s4);
-        delay(1000);
-        setSpeed(70,s3);
-        setSpeed(70,s4);
         delay(1000);
     }
   }
